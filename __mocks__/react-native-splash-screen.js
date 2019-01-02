8d7c12b9e28f5ed8d5b33908f09c884a89614f08
Jay <jayson.daji@mobile-mentor.com>
2019-01-02 14:24:18 +1300

    init commit with splashscreen

---
+++ __mocks__/react-native-splash-screen.js
@@ -0,0 +1,10 @@
+export default {
+    show: jest.fn().mockImplementation(() =>
+    {
+        console.log('show splash screen');
+    }),
+    hide: jest.fn().mockImplementation(() =>
+    {
+        console.log('hide splash screen');
+    }),
+}
