04dbe962f609b4fc1933f325110880fb4b10d70e
Jay <jayson.daji@mobile-mentor.com>
2019-01-03 14:16:30 +1300

    adding react navigation

--- android/app/src/main/java/com/portfolio/MainActivity.java
+++ android/app/src/main/java/com/portfolio/MainActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
 
+ import com.facebook.react.ReactActivityDelegate;
+ import com.facebook.react.ReactRootView;
+ import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
 
 public class MainActivity extends ReactActivity {
 
@@ -22,5 +26,14 @@ public class MainActivity extends ReactActivity {
         return "Portfolio";
     }
 
+      @Override
+      protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+          @Override
+          protected ReactRootView createRootView() {
+           return new RNGestureHandlerEnabledRootView(MainActivity.this);
+          }
+        };
+      }
 
 }
