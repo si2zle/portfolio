81d638a4b84a1a0b7ac3c556567e1dda1f29ce81
Jay <jayson.daji@mobile-mentor.com>
2019-01-03 15:12:04 +1300

    testing out some navigation icons

--- App/Containers/App.js
+++ App/Containers/App.js
@@ -9,6 +9,8 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import {createBottomTabNavigator, createAppContainer} from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 
 import SplashScreen from 'react-native-splash-screen';
 
@@ -68,8 +70,35 @@ const styles = StyleSheet.create({
     },
 });
 
-const TabNavigator = createBottomTabNavigator({
-    Home: HomeScreen, Settings: SettingsScreen,
-});
+
+const TabNavigator = createBottomTabNavigator(
+    {
+        Home: HomeScreen,
+        Settings: SettingsScreen,
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            tabBarIcon: ({focused, horizontal, tintColor}) => {
+                const {routeName} = navigation.state;
+                let IconComponent = Ionicons;
+                let iconName;
+                if (routeName === 'Home') {
+                    iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+                    // Sometimes we want to add badges to some icons.
+                    // You can check the implementation below.
+                } else if (routeName === 'Settings') {
+                    iconName = `ios-options${focused ? '' : ''}`;
+                }
+
+                // You can return any component that you like here!
+                return <IconComponent name={iconName} size={25} color={tintColor}/>;
+            },
+        }),
+        tabBarOptions: {
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+        },
+    }
+);
 
 export default createAppContainer(TabNavigator);
